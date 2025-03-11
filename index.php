aba423d7c84e2609239df9e265ab8bcbde21d5de
mattlawlis <89171568+mattlawlis@users.noreply.github.com>
2025-03-11 01:29:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_set_cookie_params([
     'lifetime' => 0,
     'path' => '/',
-    'domain' => 'stanthonyemailsignature.site',
+    'domain' => 'stanthonyemailsignature.site', //Replace with actual URL
     'secure' => true,
     'httponly' => true,
     'samesite' => 'Strict',
